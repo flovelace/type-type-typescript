e7f6b2092d851e049e9af6a97a6332f42bdccd96
fairfax <lovelace.af@gmail.com>
2022-05-14 22:00:37 -0400

    created class and constructor for typewriter

--- src/typewriter.ts
+++ src/typewriter.ts
@@ -1,5 +1,13 @@
 export default class Typewriter {
-    constructor() {
+    element: HTMLElement
+    loop: boolean
+    typingSpeed: number
+    deletingSpeed: number
 
+    constructor(element: HTMLElement, { loop = false, typingSpeed = 50, deletingSpeed = 50 } = {}) {
+        this.element = element
+        this.loop = loop
+        this. typingSpeed = typingSpeed
+        this.deletingSpeed = deletingSpeed
     }
 }
