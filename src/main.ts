6f9ec1b895e4153f892df67d84f629b84cb9bc66
fairfax <lovelace.af@gmail.com>
2022-05-14 22:39:21 -0400

    added typewriter function names

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,22 @@
 import "./style.css";
 import Typewriter from "./typewriter";
 
-const typewriter = new Typewriter(element, { loop: true })
+const typewriter = new Typewriter(document.body, { loop: true })
+
+typewriter
+.typeString("TYPE TYPE TYPE")
+.pauseFor(1000)
+.TypeString("\n\nfunctio")
+.deleteChars(7)
+.typeString("const temp")
+.pauseFor(150)
+.deleteAll(10)
+.typeString("WHY WHY WHY")
+.pauseFor(1000)
+.typeString("\n\nYES YES TEST")
+.pauseFor(1000)
+.typeString("\n\nTEST TEST TEST")
+.pauseFor(1000)
+.deleteAll(10)
+.start()
+
--- src/typewriter.ts
+++ src/typewriter.ts
@@ -1,13 +1,16 @@
 export default class Typewriter {
-    element: HTMLElement
-    loop: boolean
-    typingSpeed: number
-    deletingSpeed: number
+  element: HTMLElement
+  loop: boolean
+  typingSpeed: number
+  deletingSpeed: number
 
-    constructor(element: HTMLElement, { loop = false, typingSpeed = 50, deletingSpeed = 50 } = {}) {
-        this.element = element
-        this.loop = loop
-        this. typingSpeed = typingSpeed
-        this.deletingSpeed = deletingSpeed
-    }
+  constructor(
+    element: HTMLElement,
+    { loop = false, typingSpeed = 50, deletingSpeed = 50 } = {}
+  ) {
+    this.element = element
+    this.loop = loop
+    this.typingSpeed = typingSpeed
+    this.deletingSpeed = deletingSpeed
+  }
 }
